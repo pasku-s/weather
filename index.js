b8fedb462f946e0335dc464f9f2a8e5489a49ee7
Semion <Simon060893@users.noreply.github.com>
2016-06-05 15:24:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
+// app.use(express.static(__dirname + '/public'));
+//
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/assets/js');
+app.set('view engine', 'js');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
