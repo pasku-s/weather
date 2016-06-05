c42cc3436bc98f02e05dcbdd5c2ea86b4b5cc4f0
Semion <Simon060893@users.noreply.github.com>
2016-06-05 16:57:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 
@@ -15,6 +16,27 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+*/
+
+//Lets require/import the HTTP module
+var http = require('https');
+
+//Lets define a port we want to listen to
+const PORT= process.env.PORT || 5000;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+  response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+  //Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:%s", PORT);
 });
 
 
