c1fe946252a6c5d77c50456c5ebb9229ceef20f6
Semion <Simon060893@users.noreply.github.com>
2016-06-05 17:10:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
-/*
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
-//
+app.use(express.static(__dirname ));
+
 // views is directory for all template files
-// app.set('views', __dirname + '/assets/js');
-// app.set('view engine', 'js');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-*/
 
-//Lets require/import the HTTP module
-var http = require('https');
 
-//Lets define a port we want to listen to
-const PORT= process.env.PORT || 5000;
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-  response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-  //Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:%s", PORT);
-});
 
 
